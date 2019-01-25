c56f14d3cde041c09bb729f40db29d9c2da2f5cf
tssqzr <1214661466@qq.com>
2019-01-25 15:51:22 +0800

    过滤掉带monitor的screen

--- shell/shutdownScreen.sh
+++ shell/shutdownScreen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-for sid in $(screen -ls | grep 'neo-cli' | awk '{print $1}'); do echo $sid; done
-for sid in $(screen -ls | grep 'neo-cli' | awk '{print $1}'); do screen -S $sid -X quit; done
+for sid in $(screen -ls | grep 'neo-cli' | grep -v monitor | awk '{print $1}'); do echo $sid; done
+for sid in $(screen -ls | grep 'neo-cli' | grep -v monitor | awk '{print $1}'); do screen -S $sid -X quit; done
